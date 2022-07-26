f7a1f02b6b0ed68a86a7034a270eb2f2fdac061e
Sirpip91 <allenbradley91@hotmail.com>
2022-07-26 16:21:57 -0500

    Pthread Finished

--- part2/simple-shell-part2.c
+++ part2/simple-shell-part2.c
@@ -4,30 +4,60 @@
 // Bradley Allen
 // Adam Burris
 
+                                //PART 2 USING PTHREADS
 //includes for .c
 // ############################## INCLUDE SECTION ######################################
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/wait.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 #include <pthread.h>
+#include <errno.h>
 //define some macros for checking the paths and arguments.
 // ############################## DEFINE SECTION ########################################
-#define WHITESPACE " ,\t\n"
 #define BUFFER_SIZE 2000
-#define MAX_ARGS 64
-#define MAX_ARG_LEN 16
-
-//create variable for thread ID
-pthread_t t1;
-//global varaibles
-char *executable_path;
-//array of command arguments
-char *args[MAX_ARGS];
+#define MAX_COUNT 10 // Change MAX_COUNT as per maximum number of commands possible
 
 
-//initialize with loaded screen to show user that the shell has been loaded.
+// ##############################  Thread function  ########################################
+void ThreadFunc(char *command)
+{
+    int ret = 0;
+
+    //debugging to show errors or success
+   /* if (NULL == command)
+    {
+        printf("ERROR::Input pointer argument is NULL\n");
+        return;
+    }
+    if ('\0' == command[0])
+    {
+        printf("ERROR::Input command string is EMPTY\n");
+        return;
+    }
+
+    
+    if (0 != ret)
+    {
+        printf("ERROR::system(%s) failed. errno=%d\n", command, errno);
+    }
+    else
+    {
+        printf("SUCCESS::system(%s) succeeded\n", command);
+    }
+    */
+   //system command
+   ret = system(command);
+}
+
+
+//This is the prompt that will act as a txt/cmd line.
+void printPrompt()
+{
+    printf("Simple-Shell$  ");
+}
+//This runs once the shell has started. It acts as initialization.
 void init_shell() {
     printf("******************************************************************************\n");
     printf("  #####                                    #####                              \n");
@@ -39,158 +69,59 @@ void init_shell() {
     printf("  #####  # #    # #      ###### ######     #####  #    # ###### ###### ###### \n");
     printf("******************************************************************************\n");
     printf("\n");
-	printf("Developers: Bradley Allen, Januario Fuentes, Adam Burris\n\n");
+	printf("Group Members: Bradley Allen, Januario Fuentes, Adam Burris\n\n");
 
 }
 
 
-//This is the prompt that will act as a txt/cmd line.
-void printPrompt()
+// ##############################  Main function  ########################################
+int main () 
 {
-    printf("Simple-Shell$  ");
-}
-	//getter function for the paths of current dir
-char *getPath()
-{
-       char *path = getenv("PATH");
-	   //returns the paths of type char.
-       		 return strdup(path);
-}
-
-
-
-//THREAD1
-void *thread_1(){
-            // Execute the command with included arguments.
-            //execv(executable_path, args);
-            
-          // sprintf(executable_path);
-            system(executable_path);
-            //system(executable_path);
-            
-            
-            // The child process should now exit so that the parent can return
-            // to the prompt.
-            pthread_exit(0);
-}
-
-//main.. this starts the shell. while true get commands and search.. 
-//if command is found fork() execute command and return back to main aka parent loop.
-int main(int argc, char **argv)
-{
-    
-
-	//initialize or show that the shell is running
-	init_shell();
-	//while true run the shell and wait for commands
-    while (1)
+    //show initialization
+    init_shell();
+    //while shell is running
+    while(1)
     {
-        // Output the prompt that is shown to enter commands 
-        printPrompt();
 
-        // read input and send it to the buffer.
-        char buffer[BUFFER_SIZE] = {};
-        fgets(buffer, BUFFER_SIZE, stdin);
+    //show shell prompt
+    printPrompt();
+    //get input
+    char buffer[BUFFER_SIZE] = {};
+    fgets(buffer, BUFFER_SIZE, stdin);
+    //send the buffer to command.
+    char* command[] = {buffer, NULL};
+    //declare  variables
+    int i = 0;
+    int count = 0;
+    int ret = 0;
+    pthread_t threadId[MAX_COUNT]; // Change MAX_COUNT as per maximum number of commands possible
 
- 		//get the command.
-        char *current_token = strtok(buffer, WHITESPACE);
-		//save command to token.
-        char *command = current_token;
-		//if command is NULL continue waiting.
-        if (command == NULL)
+    //while the command is not null
+    while (NULL != command[i])
+    {
+        //create pthread to run function.
+        ret = pthread_create(&threadId[i], NULL, (void *(*)(void *))ThreadFunc, (void *)command[i]);
+        if (0 != ret)
         {
-            continue;
-        }
-
-        
-        // set first space to the command.
-        args[0] = command;
-
-        // go through the array and keep track with arg_index.
-        size_t arg_index = 0;
-
-        // While not at the end of tokens
-        while (current_token != NULL)
-        {
-			//keep reading 
-            current_token = strtok(NULL, WHITESPACE);
-			//increment.
-            args[++arg_index] = current_token;
-        }
-
-        // pointer paths getPaths.
-        char *paths = getPath();
-
-        // 0 or 1 true or false
-        int path_found = 0;
-
-        // keep track of each path.
-        char *current_path = strtok(paths, ":");
-        // executable path
-        
-			
-			//while there are paths to search
-          while (current_path != NULL)
-        {
-           // keep adding and going through paths.
-            executable_path = strdup(current_path);
-			// slash for paths.
-            strcat(executable_path, "/");
-            strcat(executable_path, command);
-
-            // if the path exe is good ==0
-            if (access(executable_path, X_OK) == 0)
-            {
-                // Set path_found will equal 1 which means found.
-                path_found = 1;
-                // break get out of loop
-                break;
-            }
-			
-			//reset the current_path to null.
-            current_path = strtok(NULL, ":");
-
-        }
-
-        	//if path could not be found then return 
-        if (path_found == 0)
-        {	
-			//command could not be found.
-            printf("Command not found\n");
-            continue;
-        }
-
-
-        // create the fork so the command can run.
-        //pid_t pid = fork();
-		
-        //INSTEAD CREATE PTHREAD TO EXECUTE COMMAND
-
-		//if pid ==0
-        /*
-        if (pid == 0)
-        {
-            // Execute the command with included arguments.
-            execv(executable_path, args);
-            // The child process should now exit so that the parent can return
-            // to the prompt.
-            exit(0);
-           
+            //printf("ERROR::pthread_create() failed for command %s. errno = %d\n", command[i], errno);
         }
         else
         {
-            // parent wait to finish child.
-            wait(NULL);
+            //printf("SUCCESS::pthread_create() succeeded for command %s\n", command[i]);
+            count++; // update i
         }
-        */
-       
-// ############################## PTHREAD SECTION ########################################
-        //run the thread which runs the command to execute.
-        pthread_create (&t1, NULL, thread_1,NULL);
-        pthread_join(t1,NULL);
-        
-        
-       
+        i++;//increment.
+    }
+
+    // pthread_join to wait till all thread are finished
+    for (i = 0; i < count; i++)
+    {
+        pthread_join(threadId[i], NULL);
+    }
+
+    //count = 0 to restart amount of threads.
+    count =0;
+
     }
 
     return 0;
